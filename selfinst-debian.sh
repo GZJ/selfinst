a931391b00084c18ddb3467194d5aad414baa07d
gzj <gzj00@outlook.com>
2024-12-03 17:56:58 +0800

    refactor: Enhance selfinst-debian.sh
    - Refactor script into modular functions for better readability and maintainability
    - Improve directory structure to support concurrent script executions
    - Add support for custom data injection during unattended installation
    - Implement flexible ISO source and destination path handling
    
    Detailed changes:
    - Separated script logic into distinct functional blocks
    - Created unique working directories using timestamps to prevent conflicts
    - Introduced a mechanism to include custom data files in the ISO
    - Ensured custom data is automatically copied to user home directory after installation
    - Enhanced error handling and input validation
    - Added support for optional custom preseed configuration

--- selfinst-debian.sh
+++ selfinst-debian.sh
@@ -1,71 +1,197 @@
 #!/bin/bash
 
-#------------------------- args -----------------------------------
-path_src_iso=$(readlink -f "$1")
-path_preseed=$(readlink -f "$2")
-path_dst_iso=$(readlink -f "$3")
+#------------------------- global variables -----------------------------------
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
 
-PATH_SCRIPT=$(dirname "$(realpath "$0")")
-PATH_WORK="$PATH_SCRIPT/iso-debian"
-PATH_SRC="$PATH_WORK/iso-decompressed"
-PATH_DST="$PATH_WORK/iso-new"
+ISO_NAME=""
 
-#------------------------- iso -----------------------------------
-iso=""
-iso_name=$(wget -q -O - https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/ | grep -oP 'debian-\d+\.\d+\.\d+-amd64-netinst\.iso' | sort -r | head -n 1)
-if [ -z "$iso_name" ]; then
-    echo "Failed to find the latest ISO name"
-    exit 1
-fi
+# Directory Structure:
+#
+# selfinst_[iso name without extension]
+# └── [timestamp_randomstring]
+#     └── extracted
+#
+# Paths explanation:
+# - ROOT: Project base directory (named after ISO)
+# - WORK: Unique directory with timestamp and random string
+# - SRC: Extracted contents directory
+# - DST: Destination path (can be any ISO path)
+PATH_ROOT=""
+PATH_WORK=""
+PATH_SRC=""
+PATH_DST=""
 
-mkdir -p $PATH_WORK $PATH_SRC $PATH_DST
-cd $PATH_WORK
+path_src_iso=""
+path_dst_iso=""
+path_preseed=""
+path_data=""
 
-if [ "$path_src_iso" = "" ] ; then
-    if [ ! -f "$PATH_WORK/$iso_name" ] ; then
-        wget https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/$iso_name
-        if [ $? -ne 0 ]; then
-            echo "Failed to download $iso_name"
-            exit 1
-        fi
-        wget https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/SHA512SUMS
-        if [ $? -ne 0 ]; then
-            echo "Failed to download SHA512SUMS"
-            exit 1
-        fi
+#------------------------- arguments -----------------------------------
+parse_arguments() {
+    usage() {
+        echo "Usage: $0 [OPTIONS]"
+        echo "Options:"
+        echo "  -s, --source-iso PATH    Path to source ISO (optional)"
+        echo "  -p, --preseed PATH       Path to preseed configuration file (optional)"
+        echo "  -d, --destination PATH   Path for destination ISO (optional)"
+        echo "  --data PATH              Path to additional data (optional)"
+        exit 1
+    }
 
-        computed_checksum=$(sha512sum $iso_name | awk '{ print $1 }')
-        expected_checksum=$(grep $iso_name SHA512SUMS | awk '{ print $1 }')
-        if [ "$computed_checksum" = "$expected_checksum" ]; then
-            echo "ISO sha512 checksum matches!"
-        else
-            echo "ISO sha512 checksum does not match!"
-            exit 1
-        fi
+    local ARGS=$(getopt -o s:p:d: --long source-iso:,preseed:,destination:,data: -n "$0" -- "$@")
+
+    if [ $? -ne 0 ]; then
+        echo -e "${RED}Error: Failed to parse arguments${NC}"
+        usage
     fi
-    iso=$PATH_WORK/$iso_name
-else
-    if [ ! -e "$path_src_iso" ] ; then
-        echo "Source ISO file $path_src_iso does not exist!"
+
+    eval set -- "$ARGS"
+
+    while true; do
+        case "$1" in
+            -s|--source-iso)
+                path_src_iso=$(readlink -f "$2")
+                shift 2
+                ;;
+            -p|--preseed)
+                path_preseed=$(readlink -f "$2")
+                shift 2
+                ;;
+            -d|--destination)
+                path_dst_iso=$(readlink -f "$2")
+                shift 2
+                ;;
+            --data)
+                path_data=$(readlink -f "$2")
+                shift 2
+                ;;
+            --)
+                shift
+                break
+                ;;
+            *)
+                echo -e "${RED}Error: Internal error!${NC}"
+                exit 1
+                ;;
+        esac
+    done
+
+    echo -e "========================"
+    echo -e "path_src_iso: $path_src_iso"
+    echo -e "path_preseed: $path_preseed"
+    echo -e "path_dst_iso: $path_dst_iso"
+    echo -e "path_data: $path_data"
+    echo -e "========================"
+}
+
+setup_directories() {
+    local path_src_iso="$1"
+    local path_dst_iso="$2"
+    local path_preseed="$3"
+    local path_data="$4"
+
+    if [ -n "$path_src_iso" ]; then
+        ISO_NAME=$(basename $path_src_iso)
+    else
+        get_current_iso_name
+    fi
+
+    if [[ -n "$path_preseed" && -n "$path_data" ]]; then
+        echo -e "${YELLOW}Warn: Both preseed and data are provided. You need to copy data from /cdrom to /target in the preseed script. Eg: (cp -r /cdrom/[data name] /target/home/[home dir]/)${NC}"
+    fi
+
+    timestamp=$(date "+%Y%m%d%H%M%S")
+    random_string=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13)
+    PATH_ROOT="$PWD/selfinst_${ISO_NAME%.*}"
+    PATH_WORK="$PATH_ROOT/${timestamp}_${random_string}"
+    PATH_SRC="$PATH_WORK/extracted"
+    PATH_DST="$PATH_WORK"
+
+    echo -e "========================"
+    echo -e "ISO_NAME: $ISO_NAME"
+    echo -e "PATH_ROOT: $PATH_ROOT"
+    echo -e "PATH_WORK: $PATH_WORK"
+    echo -e "PATH_SRC: $PATH_SRC"
+    echo -e "PATH_DST: $PATH_DST"
+    echo -e "========================"
+
+    mkdir -p "$PATH_ROOT" "$PATH_WORK" "$PATH_SRC" "$PATH_DST"  || { echo -e "${RED}Error: Failed to create directories${NC}"; exit 1; }
+}
+
+get_current_iso_name() {
+    ISO_NAME=$(wget -q -O - https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/ \
+        | grep -oP 'debian-\d+\.\d+\.\d+-amd64-netinst\.iso' \
+        | sort -r \
+        | head -n 1)
+
+    if [ -z "$ISO_NAME" ]; then
+        echo -e "${RED}Error: Failed to find the latest ISO name${NC}"
         exit 1
     fi
-    cp $path_src_iso $PATH_WORK
-    iso=$path_src_iso
-fi
+}
 
-cat $iso | bsdtar -C "$PATH_SRC" -xf -
+#------------------------- fetch iso -----------------------------------
+fetch_iso() {
+    local path_src_iso="$1"
+    local iso
+
+    cd "$PATH_ROOT"
+    if [ -z "$path_src_iso" ]; then
+        if [ ! -f "$ISO_NAME" ]; then
+            wget "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/$ISO_NAME"
+            if [ $? -ne 0 ]; then
+                echo -e "${RED}Error: Failed to download $ISO_NAME ${NC}"
+                exit 1
+            fi
+
+            wget "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/SHA512SUMS"
+            if [ $? -ne 0 ]; then
+                echo -e "${RED}Error: Failed to download SHA512SUMS${NC}"
+                exit 1
+            fi
+
+            local computed_checksum=$(sha512sum "$ISO_NAME" | awk '{ print $1 }')
+            local expected_checksum=$(grep "$ISO_NAME" SHA512SUMS | awk '{ print $1 }')
+            if [ "$computed_checksum" != "$expected_checksum" ]; then
+                echo -e "${RED}Error: ISO sha512 checksum does not match!${NC}"
+                exit 1
+            fi
+
+            echo "ISO checksum verified successfully!"
+            iso="$PATH_ROOT/$ISO_NAME"
+        else
+            iso="$PATH_ROOT/$ISO_NAME"
+        fi
+    else
+        if [ ! -e "$path_src_iso" ]; then
+            echo -e "${RED}Error: Source ISO file $path_src_iso does not exist!${NC}"
+            exit 1
+        fi
+        iso="$path_src_iso"
+    fi
+
+    cat $iso | bsdtar -C "$PATH_SRC" -xf -
+}
 
 #------------------------- preseed -----------------------------------
-preseed_hostname="selfinst"
-preseed_nameservers="114.114.114.114"
-preseed_mirror_url="mirrors.ustc.edu.cn"
-preseed_password_root="selfinstroot"
-preseed_user="g"
-preseed_password_g="selfinstg"
-preseed_package="openssh-server build-essential vim syncthing barrier"
+create_preseed_config() {
+    local path_preseed="$1"
+    local path_data_name=$(basename $2)
 
-if [ "$path_preseed" = "" ]; then
-cat << EOF > ./preseed.cfg
+    local preseed_hostname="selfinst"
+    local preseed_nameservers="114.114.114.114"
+    local preseed_mirror_url="mirrors.ustc.edu.cn"
+    local preseed_password_root="selfinstroot"
+    local preseed_user="g"
+    local preseed_password_g="selfinstg"
+    local preseed_timedate="Asia/Shanghai"
+    local preseed_package="openssh-server build-essential vim syncthing barrier"
+
+    if [ -z "$path_preseed" ]; then
+        cat << EOF > "$PATH_WORK/preseed.cfg"
 ### Localization
 d-i debian-installer/locale string en_US
 d-i console-keymaps-at/keymap select us
@@ -119,7 +245,7 @@ d-i passwd/user-password-again password $preseed_password_g
 
 ### Clock and time zone setup
 d-i clock-setup/utc boolean false
-d-i time/zone string Asia/Shanghai
+d-i time/zone string $preseed_timedate
 
 ### Partitioning
 d-i partman-auto/disk string /dev/sda
@@ -142,34 +268,75 @@ d-i grub-installer/bootdev string default
 d-i finish-install/keep-consoles boolean true
 d-i finish-install/reboot_in_progress note
 
+d-i preseed/late_command string \\
+    cp -r /cdrom/$path_data_name /target/home/$preseed_user/
 EOF
-else
-    cp $path_preseed $PATH_WORK
-fi
+    else
+        cp "$path_preseed" "$PATH_WORK"
+    fi
+}
 
-#------------------------- set decompreeed iso -----------------------------------
-chmod -R +w $PATH_SRC
-gunzip $PATH_SRC/install.amd/initrd.gz
-echo preseed.cfg | cpio -H newc -o -A -F $PATH_SRC/install.amd/initrd
-gzip $PATH_SRC/install.amd/initrd
-chmod -w -R $PATH_SRC/install.amd/
+#------------------------- prepare iso -----------------------------------
+prepare_iso() {
+    local path_data="$1"
 
-#regenerate md5sum
-cd $PATH_SRC
-chmod +w md5sum.txt
-find -follow -type f ! -name md5sum.txt -print0 | xargs -0 md5sum > md5sum.txt
-chmod -w md5sum.txt
+    cd "$PATH_WORK"
+    chmod -R +w "$PATH_SRC"
+    gunzip "$PATH_SRC/install.amd/initrd.gz"
+    echo preseed.cfg | cpio -H newc -o -A -F "$PATH_SRC/install.amd/initrd"
+    gzip "$PATH_SRC/install.amd/initrd"
+    chmod -w -R "$PATH_SRC/install.amd/"
 
-#remove startup menu
-sed -i '/default vesamenu.c32/d' isolinux/isolinux.cfg
+    if [ -n "$path_data" ] ; then
+        cp -rf "$path_data" "$PATH_SRC"
+    fi
+
+    cd "$PATH_SRC"
+    chmod +w md5sum.txt
+    find -follow -type f ! -name md5sum.txt -print0 | xargs -0 md5sum > md5sum.txt
+    chmod -w md5sum.txt
+
+    sed -i '/default vesamenu.c32/d' isolinux/isolinux.cfg
+}
 
 #------------------------- new iso -----------------------------------
-if [ "$path_dst_iso" = "" ]; then
+create_new_iso() {
+    local path_dst_iso="$1"
+    local src="$PATH_SRC"
+    local dst
+
+    cd "$PATH_SRC"
+    if [ -z "$path_dst_iso" ]; then
+        dst="$PATH_DST/preseed-$ISO_NAME"
+    else
+        dst="$path_dst_iso"
+    fi
     genisoimage -r -J -b isolinux/isolinux.bin -c isolinux/boot.cat \
                 -no-emul-boot -boot-load-size 4 -boot-info-table \
-                -o $PATH_DST/preseed-$iso_name $PATH_SRC
-else
-    genisoimage -r -J -b isolinux/isolinux.bin -c isolinux/boot.cat \
-                -no-emul-boot -boot-load-size 4 -boot-info-table \
-                -o $path_dst_iso $PATH_SRC
-fi
+                -o "$dst" "$src"
+}
+
+#------------------------- main -----------------------------------
+main() {
+    echo -e "${GREEN}---------------- args -------------------- ${NC}"
+    parse_arguments "$@"
+
+    echo -e "${GREEN}---------------- directories -------------------- ${NC}"
+    setup_directories "$path_src_iso" "$path_dst_iso" "$path_preseed" "$path_data"
+
+    echo -e "${GREEN}---------------- fetch iso -------------------- ${NC}"
+    fetch_iso "$path_src_iso"
+
+    echo -e "${GREEN}---------------- preseed -------------------- ${NC}"
+    create_preseed_config "$path_preseed" "$path_data"
+
+    echo -e "${GREEN}---------------- prepare iso -------------------- ${NC}"
+    prepare_iso "$path_data"
+
+    echo -e "${GREEN}---------------- new iso -------------------- ${NC}"
+    create_new_iso "$path_dst_iso"
+
+    echo -e "${GREEN}ISO creation completed successfully!${NC}"
+}
+
+main "$@"
